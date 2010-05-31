d8a16379731acb02376b26ececb1c7d4205e7755
Christopher Pitt <chris@sixtyseconds.co.za>
2010-05-31 14:33:04 +0200

    Updated to reflect 0.4

--- README.md
+++ README.md
@@ -8,57 +8,39 @@ This extension is a fresh look at event delegation, taking into consideration li
 How to use
 ----------
 
-    window.addEvent('domready', function() {
-                
-        var first_event = function(e)
-            {
-                console.log('first clicked!');
-            },
-            second_event = function(e)
-            {
-                console.log('second clicked!');
-                document.body.denyEvent('click', 'li.second', second_event);
-            },
-            third_event = function(e)
-            {
-                console.log('third clicked!');
-                document.body.denyEvents('mouseover', 'li.third');
-            };
-        
-        document.body.delegateEvents({
-            'click': {
-                'li.first': first_event,
-                'li.second': second_event,
-                'li.third': third_event
-            },
-            'mouseover': {
-                'li.first': function(e) {
-                    console.log('first hovered!');
-                },
-                'li.second': function(e) {
-                    console.log('second hovered!');
-                },
-                'li.third': function(e) {
-                    console.log('third hovered!');
-                }
-            }
-        });
-        
-        document.body.delegateEvent('click', {
-            'li.first': function(e)
-            {
-                console.log('second delegate on first item!');
-            }
-        });
-        
-        document.body.delegateEvent('mouseover', {
-            'li.third': function(e)
-            {
-                console.log('second delegate on third item!');
-            }
-        });
-    });
+	// A working demo has been included in the download
 	
-This code illustrates how to add event delegates to mulitple elements with a single event. The second call to document.body.delegateEvent doesn't attach a new click event but simply adds the event delegates to the current list. You can also use delegateEvents() to setup delegates for mulitple event types at the same time. The syntax is also a lot less ambiguous than the current :relay pseudo-selector.
+	document.id('container').delegateEvents({
+		'click':
+		{
+			'a.clickable': function(e)
+			{
+				e.stop();
+				alert('Told you so!');
+			},
+			'a.remove': function(e)
+			{
+				e.stop();
+				document.id('container').denyEvents('click', 'a.remove');
+				alert('My event delegation has been removed!');
+			}
+		},
+		'focus':
+		{
+			'input.focusable': function(e)
+			{
+				this.set('value', 'I have focus!');
+			}
+		},
+		'blur':
+		{
+			'input.focusable': function(e)
+			{
+				this.set('value', 'I lost focus! ;(');
+			}
+		}
+	});
+
+This code illustrates how to add event delegates to multiple elements with a single event. The second call to document.body.delegateEvent doesn't attach a new click event but simply adds the event delegates to the current list. You can also use delegateEvents() to setup delegates for multiple event types at the same time. The syntax is also a lot less ambiguous than the current :relay pseudo-selector.
 
 Please note, however, that due to current limitations in Element.match (MooTools 1.2.4) element decendency is not taken into account so you will need to be specific with id's/classes because 'ul#main > li a' is not properly handled with Element.match. This should be fixed in MooTools 2.
--- Source/event.delegation.js
+++ Source/event.delegation.js
@@ -1,116 +1,125 @@
 /*
 ---
 description: Better event delegation for MooTools.
-
 license: MIT-style
-
-authors:
-- Christopher Pitt
-- Arieh Glazer
-
-requires:
-- core/1.2.4: Element.Event
-- core/1.2.4: Selectors
-
+authors: [Christopher Pitt, Arieh Glazer, James Emerton]
 provides: [Element.delegateEvent, Element.delegateEvents, Element.denyEvent, Element.denyEvents]
-
+requires: 
+  core/1.2.4: [Element.Event, Selectors]
 ...
 */
 
-Element.implement({
-	'delegateEvent': function(type, delegates, prevent, propagate)
-	{	
-		//get stored delegates
-		var key = type + '-delegates',
-			stored = this.retrieve(key) || false;
-		
-		// if stored delegates; extend with
-		// new delegates and return self.
-		if (stored)
-		{
-            Hash.each(delegates, function(fn, selector) {
-                if (stored[selector])
-                {
-                    stored[selector].push(fn);
-                }
-                else
-                {
-                    stored[selector] = [fn];
-                }
-            });
-            
-            return this;
-		}
-		else
-		{
-            stored = new Hash();
-            Hash.each(delegates, function(fn, selector) {
-                stored[selector] = [fn];
-            });
-			this.store(key, stored);
-		}
-	
-		return this.addEvent(type, function(e)
-		{
-			// Get target and set defaults
-			var target = document.id(e.target),
-				prevent = prevent || true,
-				propagate = propagate || false
-				stored = this.retrieve(key),
-                args = arguments;
-	
-			// Cycle through rules
-            Hash.each(stored, function(delegates, selector){
-				if (target.match(selector)){
-					if (prevent) e.preventDefault();
-					if (!propagate) e.stopPropagation();
- 
-					Array.each(delegates, function(fn) {
-						if (fn.apply) fn.apply(target, args);
-					});
-				}
-			});
-            
-            return this;
-		});		
-	},
-    
-    'delegateEvents': function(delegates, prevent, propagate)
-    {
-        for (key in delegates)
-        {
-            this.delegateEvent(key, delegates[key], prevent, propagate);
-        }
-        
-        return this;
-    },
-    
-    'denyEvent': function(type, selector, fn)
-    {
-        var key = type + '-delegates',
-            stored = this.retrieve(key) || false;
-            
-        if (stored && stored[selector])
-        {
-            stored[selector].erase(fn);
-        }
-        
-        return this;
-    },
-    
-    'denyEvents': function(type, selector)
-    {
-        var key = type + '-delegates',
-            stored = this.retrieve(key) || false;
-            
-        if (stored && stored[selector])
-        {
-            delete stored[selector];
-        }
-        
-        return this;
-    }
-});
+(function(context) {
 
-Element.alias('delegateEvent','relayEvent');
-Element.alias('delegateEvents','relayEvents');
+	var each = function(collection, fn, context)
+	{
+		for (var key in collection)
+		{
+			if (collection.hasOwnProperty(key))
+			{
+				fn.apply((context || this), [key, collection[key]]);
+			}
+		}
+		return collection;
+	};
+
+	Element.implement({
+		'delegateEvent': function(type, delegates, prevent, propagate)
+		{
+			//get stored delegates
+			var self = this,
+				key = 'delegates:' + type,
+				stored = this.retrieve(key) || false,
+				handler = function(e)
+				{
+					// Get target and set defaults
+					var target = document.id(e.target),
+						prevent = prevent || true,
+						propagate = propagate || false
+						stored = self.retrieve(key),
+						args = arguments;
+
+					// Cycle through rules
+					each(stored, function(selector, delegates) {
+						if (target.match(selector))
+						{
+							if (prevent) e.preventDefault();
+							if (!propagate) e.stopPropagation();
+							each(delegates, function(key, fn) {
+								fn.apply && fn.apply(target, args);
+							});
+						}
+					}, self);
+					return self;
+				};
+
+			// if stored delegates; extend with new delegates and return self.
+			if (stored)
+			{
+				each(delegates, function(selector, fn) {
+					(stored[selector]) ? stored[selector].push(fn) : stored[selector] = [fn];
+				});
+				return self;
+			}
+			else
+			{
+				stored = {};
+				each(delegates, function(selector, fn) {
+					stored[selector] = [fn];
+				});
+				self.store(key, stored);
+			}
+
+			// if event type is focus/blur then shim for delegation.
+			if (/focus|blur/.test(type))
+			{
+				var wrapper = function(e)
+					{
+						e = new Event(e, self.getWindow());
+						handler.call(self, e);
+					};
+
+				if (Browser.Engine.trident)
+				{
+					// aliased events for (smell)ie...
+					self.attachEvent(type == 'focus' ? 'onfocusin' : 'onfocusout', wrapper);
+				}
+				else
+				{
+					self.addEventListener(type, wrapper, true);
+				}
+			}
+			else
+			{
+				self.addEvent(type, handler);
+			}
+			return self;
+		},
+
+		'delegateEvents': function(delegates, prevent, propagate)
+		{
+			each(delegates, function(key, delegate) {
+				this.delegateEvent(key, delegate, prevent, propagate);
+			}, this);
+			return this;
+		},
+
+		'denyEvent': function(type, selector, fn)
+		{
+			var stored = this.retrieve('delegates:' + type) || false;
+			stored && stored[selector] && stored[selector].erase(fn);
+			return this;
+		},
+
+		'denyEvents': function(type, selector)
+		{
+			var stored = this.retrieve('delegates:' + type) || false;
+			stored && stored[selector] && delete stored[selector];
+			return this;
+	    }
+	});
+
+	Element.alias('delegateEvent','relayEvent');
+	Element.alias('delegateEvents','relayEvents');
+
+})(window ? window : this);
