15fc25a4597e184ca866cc717150ed34bee0db20
Chris <chris@sixtyseconds.co.za>
2011-04-29 09:02:25 +0200

    Updated examples.

--- examples/demo.js
+++ examples/demo.js
@@ -2,29 +2,29 @@ window.addEvent('domready', function() {
 	document.id(document.body).delegateEvents({
 		'click': {
 			'a.clickable': function() {
-				log('a.clickable clicked');
+				console.log('a.clickable clicked');
 			},
 			'a.remove': function() {
 				document.id(document.body).denyEvents('click', 'a.remove');
-				log('a.remove clicked. it now has no click event');
+				console.log('a.remove clicked. it now has no click event');
 			},
 			'self': function() {
-				log('self (document.body) clicked.');
+				console.log('self (document.body) clicked.');
 			}
 		},
 		'focus': {
 			'input.focusable': function() {
-				log('input.focusable focused');
+				console.log('input.focusable focused');
 			}
 		},
 		'blur': {
 			'input.focusable': function() {
-				log('input.focusable blurred');
+				console.log('input.focusable blurred');
 			}
 		},
 		'change': {
 			'select.changeable': function() {
-				log('select.changeable changed');
+				console.log('select.changeable changed');
 			}
 		}
 	});
